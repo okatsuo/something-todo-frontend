7a0c1ad690f78fbf373182ac84a3d5c56311776d
okatsuo <rafael.goncanna@gmail.com>
2021-09-12 18:10:01 -0300

    refactor: create functions to Users

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,17 +1,15 @@
 import { Container } from '@material-ui/core'
 import Router from 'next/router'
-import { parseCookies, destroyCookie } from 'nookies'
-import { jwtDecode } from '../../utils/JWT'
+import { UserLoggedInfo, UserLogout } from '../../utils/user-account-stuff'
 import { CustomLink } from '../basics/link'
 import { Text } from '../basics/text'
 import * as Styles from './styles'
 
 const Header = () => {
-  const cookies = (parseCookies()).t_user
-  const user = jwtDecode(cookies)
+  const user = UserLoggedInfo()
 
   const logout = async () => {
-    destroyCookie(null, 't_user')
+    UserLogout()
     await Router.push('/login')
   }
 
