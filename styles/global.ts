e50386ebe4fe59e35fb1f5120be3a9e37dc39827
okatsuo <rafael.goncanna@gmail.com>
2021-09-16 08:59:03 -0300

    refactor: remove linear-gradient background

--- styles/global.ts
+++ styles/global.ts
@@ -4,7 +4,7 @@ export default createGlobalStyle`
   html,
   body {
     color: #FFFFFF;
-    background: linear-gradient(to right bottom, #37383c, #323337, #2e2f33, #292a2e, #25262a);
+    background: #37383c;
     padding: 0;
     margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
