d99499d68fc0eea968cf9c3fa76126de503c462b
okatsuo <rafael.goncanna@gmail.com>
2021-09-18 23:08:09 -0300

    refactor: move AddTodo functions to correct file

--- src/components/Todo/addTodo/index.tsx
+++ src/components/Todo/addTodo/index.tsx
@@ -1,26 +1,59 @@
 import { InputAdornment } from '@material-ui/core'
 import { AddCircle } from '@material-ui/icons'
 import React from 'react'
+import { initializeApollo } from '../../../graphql/client'
+import { CREATE_TODO } from '../../../graphql/mutations/createTodo'
 import * as Styles from './styles'
 
 interface AddTodoProps {
-  value: string
+  newTodo: string
+  userId: string
+  userTodos: any[]
   setNewTodo: (value: string) => void
-  handleKeyboardEvent: (value: React.KeyboardEvent<HTMLDivElement>) => void
-  handleRegisterTodo: () => void
+  setUserTodo: (values: any[]) => void
 }
 
 const AddTodo = (props: AddTodoProps) => {
-  const { value, setNewTodo, handleKeyboardEvent, handleRegisterTodo } = props
+  const { setNewTodo, setUserTodo, newTodo, userId, userTodos } = props
+
+  const apolloClient = initializeApollo()
+
+  const handleRegisterTodo = async () => {
+    if (!newTodo) return
+    const todo = {
+      name: newTodo,
+      description: '',
+      active: true,
+      user_id: userId
+    }
+    try {
+      const { data } = await apolloClient.mutate({
+        mutation: CREATE_TODO,
+        variables: todo
+      })
+      setNewTodo('')
+      const { todoCreate } = data
+      setUserTodo([...userTodos, todoCreate])
+    } catch (error) {
+      console.error('deu erro...', error)
+    }
+  }
+
+  const handleKeyboardEvent = async ({ key }: React.KeyboardEvent<HTMLDivElement>) => {
+    if (key === 'Enter') {
+      await handleRegisterTodo()
+    }
+  }
+
   return (
     <Styles.InputAddTodo
       variant='filled'
       label='adicionar uma nova tarefa'
       style={{ paddingRight: '15px' }}
       fullWidth
-      value={value}
+      value={newTodo}
       onChange={(event) => setNewTodo(event.target.value)}
-      onKeyPress={async (target) => handleKeyboardEvent(target)}
+      onKeyPress={async (target) => await handleKeyboardEvent(target)}
       InputProps={{
         endAdornment: (
           <InputAdornment position="end">
--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -1,16 +1,13 @@
-import { useQuery } from '@apollo/client'
-import { Box, Container, List } from '@material-ui/core'
-import React, { KeyboardEvent, useEffect, useState } from 'react'
-import { initializeApollo } from '../../graphql/client'
-import { CREATE_TODO } from '../../graphql/mutations/createTodo'
-import { USER_TODO } from '../../graphql/queries/userTodos'
 import { UserLoggedInfo } from '../../utils/user-account-stuff'
-import AddTodo from './addTodo'
-import ListTodo from './listTodo'
+import { USER_TODO } from '../../graphql/queries/userTodos'
+import { Box, Container, List } from '@material-ui/core'
+import React, { useEffect, useState } from 'react'
+import { useQuery } from '@apollo/client'
 import * as Styles from './styles'
+import ListTodo from './listTodo'
+import AddTodo from './addTodo'
 
 const Todo = () => {
-  const apolloClient = initializeApollo()
   const [newTodo, setNewTodo] = useState('')
   const [userTodos, setUserTodo] = useState<any[]>([])
   const userLogged = UserLoggedInfo()
@@ -24,43 +21,17 @@ const Todo = () => {
     }
   }, [userTodo])
 
-  const handleRegisterTodo = async () => {
-    if (!newTodo) return
-    const todo = {
-      name: newTodo,
-      description: '',
-      active: true,
-      user_id: userLogged?.id
-    }
-    try {
-      const { data } = await apolloClient.mutate({
-        mutation: CREATE_TODO,
-        variables: todo
-      })
-      setNewTodo('')
-      const { todoCreate } = data
-      setUserTodo([...userTodos, todoCreate])
-    } catch (error) {
-      console.error('deu erro...', error)
-    }
-  }
-
-  const handleKeyboardEvent = async ({ key }: KeyboardEvent) => {
-    if (key === 'Enter') {
-      await handleRegisterTodo()
-    }
-  }
-
   return (
     <Styles.Wrapper>
       <Container maxWidth='md'>
         <Styles.Content>
           <Box m={3} justifyContent='center' alignItems='center' display='flex' flexDirection='row'>
             <AddTodo
-              handleKeyboardEvent={handleKeyboardEvent}
-              handleRegisterTodo={handleRegisterTodo}
+              setUserTodo={setUserTodo}
               setNewTodo={setNewTodo}
-              value={newTodo}
+              userId={userLogged?.id}
+              userTodos={userTodos}
+              newTodo={newTodo}
             />
           </Box>
           <List>
